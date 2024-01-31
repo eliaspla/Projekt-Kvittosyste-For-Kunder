adceacbdab1d19864f6bd75add9f1523944742a7
eliaspla <148338620+eliaspla@users.noreply.github.com>
2024-01-31 12:03:48 +0000

    first init

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-Spacing = "                                     "
+
+from typing import DefaultDict
+
+
 lovely_loveseat_description = "Härlig Kärlekssoffa. Tuftad polyesterblandning på trä. 81 cm hög x 102 cm bred x 76 cm djup. Röd eller vit."
 lovely_loveseat_price = 254.00
 stylish_settee_description = "Stilfull Bänksoffa. Konstläder på björk. 75 cm hög x 139 cm bred x 71 cm djup. Svart."
@@ -10,7 +13,6 @@ customer_one_total = 0
 customer_one_itemization = ""
 customer_one_total += lovely_loveseat_price
 customer_one_itemization += lovely_loveseat_description
-customer_one_itemization += Spacing
 customer_one_total += luxurious_lamp_price
 customer_one_itemization += luxurious_lamp_description
 customer_one_tax = customer_one_total * sales_tax
